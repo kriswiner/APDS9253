e4d3abd4abd39455c51dbb30117f91c930cb8b08
Kris Winer <kriswiner@users.noreply.github.com>
2021-08-04 15:45:39 -0700

    Update APDS9253_Ladybug.v01.ino

--- APDS9253_Ladybug.v01/APDS9253_Ladybug.v01.ino
+++ APDS9253_Ladybug.v01/APDS9253_Ladybug.v01.ino
@@ -26,10 +26,10 @@ volatile bool APDS9253_intFlag = false;
 
 uint8_t RGB_mode = RGBiR; // Choice is ALSandIR (green and IR channels only) or RGBiR for all four channels
 //rate has to be slower than ADC settle time defines by resolution
-// Bright sunlight is maximum ~25 klux so choose fastest rate (40 kHz) and minimum resolution (16 bits)
-// that allows ~24klux maximum to be measured; this saves the most power
+// Bright sunlight is maximum ~25 klux so choose gain of 6x and minimum resolution (16 bits)
+// that allows ~24 klux maximum to be measured;  a 1 Hz rate costs ~114 uA * 25/1000 ~ 3 uA
 uint8_t LS_res = res16bit; // Choices are res20bit (400 ms), res19bit, res18bit (100 ms, default), res17bit, res16bit (25 ms).
-uint8_t LS_rate = rate40Hz; // Choices are rate40Hz (25 ms), rate20Hz, rate10Hz (100 ms, default), rate5Hz, rate2_5Hz (400 ms), rate1Hz, rate0_5Hz
+uint8_t LS_rate = rate1Hz; // Choices are rate40Hz (25 ms), rate20Hz, rate10Hz (100 ms, default), rate5Hz, rate2_5Hz (400 ms), rate1Hz, rate0_5Hz
 uint8_t LS_gain = gain6; // Choices are gain1, gain3 (default), gain6, gain9, gain18
 uint32_t RGBiRData[4] = {0, 0, 0, 0}; // red, green, blue, ir counts
 
